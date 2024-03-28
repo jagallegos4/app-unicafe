ebe877f0e04fc76200af0028731c82206537bb47
Andres Gallegos <gallegos-andres@hotmail.com>
2024-03-27 22:18:39 -0500

    paso 1

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+//import './App.css'
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+
+
+  return (
+    <div>
+      <h1>Giveme feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad+1)}>Bad</button>
+      <h1>statics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+
+    </div>
+    // <>
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </>
+  )
+}
+
+export default App
